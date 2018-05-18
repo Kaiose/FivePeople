5d52265095d231f77b060778ce712673be72a504
Kimkinam <kaiose30@gmail.com>
2018-05-18 21:09:51 +0900

    Manager

---
+++ SpriteManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameManager;
+
+public class SpriteManager : MonoBehaviour {
+
+    static protected SpriteManager s_SpriteInstance;
+    static public SpriteManager SpriteInstance { get { return s_SpriteInstance; } }
+
+
+    private SpriteRenderer[] spriteRenderer;
+
+    [SerializeField]
+    private GameObject StageGroup;
+    [SerializeField]
+    private Sprite StageClear;
+    [SerializeField]
+    private Sprite StageOpen;
+    [SerializeField]
+    private Sprite StageUnOpen;
+    // Use this for initialization
+
+   void Awake()
+    {
+        s_SpriteInstance = this;
+    }
+    
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StageSpriteUpdate(string stage)
+    {
+        print("StartStageSpriteUpdate");
+        spriteRenderer = StageGroup.GetComponentsInChildren<SpriteRenderer>();
+
+        bool StageState = false;
+
+        foreach (SpriteRenderer child in spriteRenderer)
+        {
+
+            if(child.gameObject.name.CompareTo(stage) == 0 && StageState == false)
+            {
+                child.sprite = StageOpen;
+                StageState = true;
+            }
+            if (StageState == false)
+            {
+                child.sprite = StageClear;
+            }
+
+        }
+
+    }
+
+}
