40358be2f279abfb0287e7361dc53c07e1366554
Kimkinam <kaiose30@gmail.com>
2018-05-18 22:56:52 +0900

    Manager

---
+++ SettingButtonClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameManager;
+public class SettingButtonClick : MonoBehaviour {
+
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseUp()
+    {
+        print("SettingButtonClick");
+        EventManager.EventInstance.CallFadeOut();
+    }
+}
