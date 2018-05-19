7b86f67ad3577123459aba387e7788789f48f719
jhj90331 <37953786+jhj90331@users.noreply.github.com>
2018-05-19 18:13:14 +0900

    스테이지 입장화면
    스테이지 입장화면

---
+++ StageSelectManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectManager : MonoBehaviour {
+
+	private SpriteRenderer[] spriteRenderer;
+
+	[SerializeField]
+	private short CurrentStage;
+	[SerializeField]
+	private GameObject StageSelect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string stage = "stage";
+		string stageNumber = CurrentStage.ToString();
+		stage += stageNumber;
+		print(stage);
+		StageSelectNumber(stage);
+	}
+	
+	public void StageSelectNumber(string stage)
+	{
+		spriteRenderer = StageSelect.GetComponentsInChildren<SpriteRenderer>();
+
+		foreach (SpriteRenderer child in spriteRenderer)
+		{
+			if(child.gameObject.name.CompareTo(stage) == 0)
+			{				
+				child.enabled = true;
+			}
+		}
+	}
+}
