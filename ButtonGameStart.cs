fc0903f4df86990d4130a235891032e92c9a8c68
jhj90331 <37953786+jhj90331@users.noreply.github.com>
2018-05-19 18:13:14 +0900

    스테이지 입장화면
    스테이지 입장화면

---
+++ ButtonGameStart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonGameStart : MonoBehaviour {
+
+	[SerializeField]
+	private SpriteRenderer StartCircle;
+
+	private ButtonBooster booster;
+
+	bool boosted = false;
+
+	// Use this for initialization
+	void Start () {
+		StartCircle.enabled = false;
+		booster = GetComponent<ButtonBooster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		booster.boosterActivated = boosted;
+	}
+
+	private void OnMouseEnter()
+	{
+		StartCircle.enabled = true;
+	}
+
+	private void OnMouseExit()
+	{
+		StartCircle.enabled = false;
+	}
+
+	private void OnMouseDown()
+	{
+		/*	boosted를 확인하고 게임 시작	 */
+	}
+}
