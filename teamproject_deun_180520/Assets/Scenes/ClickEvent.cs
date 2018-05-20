4a4e57f874215b1c33dee839e9a9c4bfc2f4b977
SE DEUN LEE <godsatm@naver.com>
2018-05-20 20:13:54 +0900

    180520 작업파일
    180520 작업파일

---
+++ teamproject_deun_180520/Assets/Scenes/ClickEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEvent : MonoBehaviour {
+
+    public GameObject DestroyCanvas;
+    public GameObject ActiveCanvas;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseUp()
+    {
+       Debug.Log("마우스 클릭 됨");
+        DestroyCanvas.SetActive(false);
+        ActiveCanvas.SetActive(true);
+       
+    }
+}
