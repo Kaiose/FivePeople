ca26deb8a225b96db7b1c00bceac998ceb132d02
BAEK-Hwan-Gi <abcy100@nate.com>
2018-05-26 22:02:01 +0900

    Mouse Cursor & Score
    점수가 생긴다면 바로 적용시킬 수 있도록,
    
    그리고 마우스 올리면 색깔 변함

---
+++ _Real_Alchemist_/Assets/Script/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    // 블록이 파괴되는 부분에 AddScore함수를 추가해주면, 위에 스코어 창에 점수가 오르는 것을 확인할 수 있다.
+
+    /*
+     *  스코어가 오르는 곳에 LowCanvas 속에 있는 Combo들을 활성화 시켜주는 코드를 넣어야 한다. 
+    */
+    public static ScoreManager instance; 
+    public Text scoreText; 
+    private int score = 0;
+
+
+    void Awake()
+    {
+        if (!instance) 
+            instance = this; 
+    }
+
+    public void AddScore(int num) 
+    {
+        score += num; 
+        scoreText.text += score; 
+    }    
+}
---
+++ _Real_Alchemist_/Assets/Script/StarOn.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarOn : MonoBehaviour {
+    /*
+     *  HpBar와 거의 유사한 코드지만, 별이 줄어드는 것을 구현하기 위해서 새로 만들었다. 
+     *  똑같이 bool을 이용해서 켰다가 껐다가 한다. 
+    */
+    Image fillIng;
+
+    // 점수에 따라 늘어나는 게이지의 량
+    private float scoreAmt = 10;
+    float score;
+
+    public GameObject star;
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+
+    private bool onoff;
+
+
+    void Start()
+    {
+        onoff = false;
+        fillIng = this.GetComponent<Image>();
+        score = scoreAmt;
+    }
+
+    /*
+        나중에 게임이 만들어 진다면, time을 score로 바꾸면 될 것.
+    */
+    void FixedUpdate()
+    {
+        if (onoff == true)
+        {
+            if (score >= 0)
+            {
+                // score -= 점수가 오르는 만큼 적절하게;
+                fillIng.fillAmount = score / scoreAmt;
+                if (fillIng.fillAmount < 0.952 && fillIng.fillAmount > 0.229)
+                {
+                    Destroy(star);
+                    star1.SetActive(true);
+                }
+                if (fillIng.fillAmount < 0.229 && fillIng.fillAmount > 0.039)
+                {
+                    Destroy(star1);
+                    star2.SetActive(true);
+                }
+                if (fillIng.fillAmount < 0.039)
+                {
+                    Destroy(star2);
+                    star3.SetActive(true);
+                }
+
+
+            }
+        }       
+    }
+
+    public void Off()
+    {
+        onoff = false;
+    }
+
+    public void On()
+    {
+        onoff = true;
+    }
+}
