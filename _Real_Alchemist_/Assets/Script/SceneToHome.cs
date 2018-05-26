42d35f78dde9bb4141a8f1315fa751e7756bfa4b
BAEK-Hwan-Gi <abcy100@nate.com>
2018-05-26 22:02:01 +0900

    Mouse Cursor & Score
    점수가 생긴다면 바로 적용시킬 수 있도록,
    
    그리고 마우스 올리면 색깔 변함

---
+++ _Real_Alchemist_/Assets/Script/SceneToHome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneToHome : MonoBehaviour {
+    /*
+        이 코드는 아래 "Home"이라고 되어 있는 Scene으로 이동시킨다.
+        지금은 '포기하기'버튼을 누르면 이동되도록 만들었다.
+    */
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void ReStart()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+}
